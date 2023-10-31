26c629fc3ba1097d8580ea11be9ee17a7540bafc
James-Sant <127257078+James-Sant@users.noreply.github.com>
2023-10-31 13:15:31 -0600

    visualizations

--- Code/Functions.R
+++ Code/Functions.R
@@ -1,4 +1,11 @@
-func <- function(){
-  print("Hello")
+#GGplot histogram
+ggplot_hist <- function(df,x){
+  return(ggplot(df, aes(x = df$x))+
+           geom_histogram()
+  )
 }
 
+ggplot_bar <- function(df,x,y){
+  return(ggplot(df, aes(x=x,y=y))+
+           geom_bar(stat = "identity"))
+}
