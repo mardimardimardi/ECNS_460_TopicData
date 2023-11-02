dd881d2b2211a8a98ed303898d18fc0423756e30
James-Sant <127257078+James-Sant@users.noreply.github.com>
2023-11-02 16:13:04 -0600

    Working on synthetic control

--- Code/Functions.R
+++ Code/Functions.R
@@ -9,3 +9,11 @@ ggplot_bar <- function(df,x,y){
   return(ggplot(df, aes(x=x,y=y))+
            geom_bar(stat = "identity"))
 }
+
+healthrank_to_yearly <- function(df){
+  df <- df %>%
+    group_by(year) %>%
+    summarise(across(where(is.numeric), ~ mean(., na.rm = TRUE))) %>%
+    select(-c("X", "fips"))
+  return(df)
+}
